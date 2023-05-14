d9c4ddb738e59a101ead8237ab76937a082f52ef
Magma5gater <132434007+Magma5gater@users.noreply.github.com>
2023-05-14 08:04:13 +0800

    Update StridePrefetcher.scala

--- src/main/scala/prefetcher/StridePrefetcher.scala
+++ src/main/scala/prefetcher/StridePrefetcher.scala
@@ -5,18 +5,33 @@ import chisel3.util._
 import chisel3.stage.{ChiselStage, ChiselGeneratorAnnotation}
 
 // TODO: update this module to implement stride prefetching.
-class StridePrefetcher(M: Int, N: Int) extends Module {
-  val io = IO(new StridePrefetcherIO(M, N))
-
-  io.out := DontCare
-
-  for (i <- 0 until M) {
-    for (j <- 0 until N) {
-      var sum = 0.S(32.W)
-
-      sum = io.a(i * N + j) + io.b(i * N + j)
-
-      io.out(i * N + j) := sum
+class StridePrefetcher(val addressWidth: Int, val pcWidth: Int) extends Module {
+  val io = IO(new Bundle {
+    val pc = Input(UInt(pcWidth.W))
+    val address = Input(UInt(addressWidth.W))
+    val prefetch = Output(UInt(addressWidth.W))
+    val prefetch_valid = Output(Bool())
+  })
+  val depth = 8.U
+  val pcSheet = Reg(Vec(depth, UInt(pcWidth.W)))
+  val addressSheet = Reg(Vec(depth, UInt(addressWidth.W)))
+  val strideSheet = Reg(Vec(depth, UInt(addressWidth.W)))
+  val Areg = RegInit(0.U(depth.W))
+  val len = RegInit(0.U(depth.W))
+  val find = RegInit(false.B)
+  find := false.B
+  for (i <- 0 until len) {
+    if(pc===pcSheet((i+Areg)%depth){
+      if(address - addressSheet((i+Areg)%depth)===strideSheet((i+Areg)%depth)){
+        io.out.prefetch := address + strideSheet((i+Areg)%depth)
+        io.out.prefetch_valid := true.B
+        find := true.B
+        break
+      }
     }
+  }
+  if(find){
+    Areg := Areg + 1.U
+    
   }
 }
